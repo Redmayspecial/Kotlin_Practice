68c3e2afe2902076885746f3e78a72cf0e77a968
Glenn Oliver <glennoliver@outlook.com>
2018-09-19 16:26:11 -0400

    Learning functions

--- Functions.kt
+++ Functions.kt
@@ -1,11 +1,40 @@
 package Kotlin_Practice
 
+import java.util.*
+
+// No parameters, no return value
 fun helloWorld() {
     println("Hello World!")
-    //Testing version control statement
 
+}
+// "Kotlin" -> K o t l i n"
+
+// 1 Parameter, but no return value
+fun printWithSpaces(text: String) {  //specifies the type of the value expected to be passed into the function
+    for (char in text){
+        print(char + " ")
+    }
+    println()
+}
+
+// No Parameters, returns Date
+fun getCurrentDate(): Date { //putting the type behind the function indicates the type of value to be returned
+    return Date()
+
+}
+
+// 2 Parameters, returns Int
+fun max(a: Int, b: Int): Int  {
+    if (a>=b) {
+        return a
+    } else {
+        return b
+    }
 }
 
 fun main(args: Array<String>) {
     helloWorld()
+    printWithSpaces("Kotlin is awesome")
+    println(getCurrentDate())
+    println(max(17,42))
 }
---
+++ Reverse.kt
@@ -0,0 +1,8 @@
+package Kotlin_Practice
+
+
+fun main(args: Array<String>) {
+    val numbers = listOf(1,2,3,6,8,9)
+}
+
+fun reverse(list:)
