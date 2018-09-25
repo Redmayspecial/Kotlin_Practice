51a1b03643494e78c059d9f65745202b106760e2
Glenn Oliver <glennoliver@outlook.com>
2018-09-25 14:46:23 -0400

    Bitwise comparisons

--- OO_Color.kt
+++ OO_Color.kt
@@ -8,10 +8,11 @@ package Kotlin_Practice
 
 enum class Color(val rgb: Int) {
     RED(0xFF0000),GREEN(0x00FF00),BLUE(0x0000FF), YELLOW(0xFFFF00);
-    //The enum requires the ; separator if followed by a method
+    //The enum requires the ; separator IF followed by a method
 
     fun containsRed(): Boolean{
-        return false
+        //do a bitwise comparison to see if the number compared contains an number in the red spectrum
+        return this.rgb and 0xFF0000 != 0
     }
 }
 
@@ -36,5 +37,28 @@ fun main(args: Array<String>) {
     // binary numbers
     // 0..1 -> 0b1010
 
-    printlin(0b)
+    println(0b1010)
+    println(0b00001010)
+    println(0b11111111 == 0xFF)  // = 255
+    println(0b11111111_00000000_00000000 == 0xFF0000)
+
+    // and operator
+    // 1 and 1 == 1
+    // 1 and 0 == 0
+    // 0 and 1 == 0
+    // 0 and 0 == 0
+    //
+    //     1101
+    // and 1011
+    //-----------
+    //  =  1001
+
+    println(0b1101 and 0b1011)
+
+    // 0x01CAFE and 0xFF0000 == x010000
+    println(Color.RED.containsRed())
+    println(Color.GREEN.containsRed())
+    println(Color.BLUE.containsRed())
+    println(Color.YELLOW.containsRed())
+
 }
