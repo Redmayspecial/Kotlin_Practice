21bd8ee585c2c2b3e8f736e61abc897ff27899e1
Glenn Oliver <glennoliver@outlook.com>
2018-09-26 16:06:10 -0400

    Generic Functions

--- Stack.kt
+++ Stack.kt
@@ -2,18 +2,53 @@ package Kotlin_Practice
 
 // DRY  Don't Repeat Yourself, more error prone and harder to upkeep.
 
-class Stack<E> {  / E for element  E T K V
+// class Stack<E> {  / E for element  E T K V
+class Stack<E>(vararg val items: E) {  // vararg allows for multiple arguments of type
 
-    fun push() {
+    val elements = items.toMutableList()
+
+
+    fun push(element: E) {
         // val array = intArrayOf(1,2,3)
-        val array = arrayOf(1,2,3)  //Kotlin has inferred the type of array.
+        // val array = arrayOf(1,2,3)  //Kotlin has inferred the type of array.
+
+        // arrayListOf(1,2,3,4)
+        // val stack = Stack(1,2,3,4)
+        elements.add(element)
     }
 
-    fun pop() {
+    fun pop(): E? {
+        if (!isEmpty()) {
+            return elements.removeAt(elements.size -1)
+        }
+        return null
 
     }
+
+    fun isEmpty(): Boolean {
+        return elements.isEmpty()
+    }
+}
+
+fun <T> stackOf(vararg elements: T): Stack<T>{
+    return Stack(*elements)  // *makes the stack of elements a vararg
 }
 
 // class IntStack : Stack() {
 
+fun main(args: Array<String>) {
+    val stack = Stack(3,5,2,8)
+    // stack.push(11)
+
+
+    val stack2 = stackOf("Hi", "Hey", "Hello")
+    for (i in 1..3){
+        println(stack2.pop())
+    }
+
+    println(stack.pop())
+    println(stack.pop())
+    println(stack.pop())
+    println(stack.pop())
+    println(stack.pop()) //should cause elements beyond range - null
 }
