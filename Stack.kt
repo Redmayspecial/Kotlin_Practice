c962ae849abf92d4a5a3552315d439cd659456ef
Glenn Oliver <glennoliver@outlook.com>
2018-09-26 15:27:39 -0400

    Dry concept

---
+++ Stack.kt
@@ -0,0 +1,19 @@
+package Kotlin_Practice
+
+// DRY  Don't Repeat Yourself, more error prone and harder to upkeep.
+
+class Stack<E> {  / E for element  E T K V
+
+    fun push() {
+        // val array = intArrayOf(1,2,3)
+        val array = arrayOf(1,2,3)  //Kotlin has inferred the type of array.
+    }
+
+    fun pop() {
+
+    }
+}
+
+// class IntStack : Stack() {
+
+}
